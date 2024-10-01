2e1fa51128845802b37f0e0f11ece271a68ca14f
Keiffer da Silva <keifferdasilva@gmail.com>
2024-10-01 16:15:56 -0600

    Add register test

--- src/tests/authRouter.test.js
+++ src/tests/authRouter.test.js
@@ -2,18 +2,41 @@ const request = require('supertest')
 const app = require('../service');
 const {Role, DB} = require("../database/database");
 
-async function createUser(){
-   const newUser = { name: "keiffer", email: "k@jwt.com", password: "admin", roles: [{ role: Role.Admin }] };
-   await DB.addUser(newUser);
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+
+    await DB.addUser(user);
+
+    user.password = 'toomanysecrets';
+    return user;
 }
 
 test('login', async() =>{
-    await createUser();
-    const login = {email: "k@jwt.com", password: "admin"};
+    const user = await createAdminUser();
+    const login = {email: user.email, password: user.password};
     const loginRes = await request(app).put('/api/auth').send(login);
     expect(loginRes.statusCode).toBe(200);
-    expect(loginRes.body.user.email).toMatch("k@jwt.com");
+    expect(loginRes.body.user.email).toMatch(user.email);
     expect(loginRes.body.user.roles).toEqual([{role: "admin"}]);
     expect(loginRes.body).toHaveProperty('token');
-    expect(loginRes.body.user.name).toMatch('keiffer');
+    expect(loginRes.body.user.name).toMatch(user.name);
+});
+
+test('register', async() =>{
+    let user = { password: 'toomanysecrets'};
+    user.name = randomName();
+    user.email = user.name + '@jwt.com';
+
+    const registerRes = await request(app).post('/api/auth').send(user);
+    expect(registerRes.statusCode).toBe(200);
+    expect(registerRes.body.user.email).toMatch(user.email);
+    expect(registerRes.body.user.name).toMatch(user.name);
+    expect(registerRes.body).toHaveProperty('token');
+    expect(registerRes.body.user.roles).toEqual([{role: "diner"}]);
 });
