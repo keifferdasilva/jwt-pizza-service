9d8d6150afc1fd4660ef49b089ad8a9489995ce8
Keiffer da Silva <keifferdasilva@gmail.com>
2024-10-01 15:00:01 -0600

    Added dummy user for testing

--- src/tests/authRouter.test.js
+++ src/tests/authRouter.test.js
@@ -1,12 +1,19 @@
 const request = require('supertest')
 const app = require('../service');
+const {Role, DB} = require("../database/database");
+
+beforeAll( async() =>{
+   const newUser = { name: "keiffer", email: "k@jwt.com", password: "admin", roles: [{ role: Role.Admin }] };
+    DB.addUser(newUser).then((r) => console.log('created user: ', r));
+});
 
 test('login', async() =>{
-    const login = {email: "a@jwt.com", password: "admin"};
+    const login = {email: "k@jwt.com", password: "admin"};
     const loginRes = await request(app).put('/api/auth').send(login);
     expect(loginRes.statusCode).toBe(200);
-    expect(loginRes.body.user.id).toBe(1);
-    expect(loginRes.body.user.email).toMatch("a@jwt.com");
+    expect(loginRes.body.user.id).toBe(2);
+    expect(loginRes.body.user.email).toMatch("k@jwt.com");
     expect(loginRes.body.user.roles).toEqual([{role: "admin"}]);
     expect(loginRes.body).toHaveProperty('token');
+    expect(loginRes.body.user.name).toMatch('keiffer');
 });
