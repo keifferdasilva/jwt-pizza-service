7e2a28d7c7560a27bdf7592c5475074728ed83ba
Keiffer da Silva <keifferdasilva@gmail.com>
2024-10-01 14:39:38 -0600

    Fix login test Change ci order

--- src/tests/authRouter.test.js
+++ src/tests/authRouter.test.js
@@ -3,7 +3,7 @@ const app = require('../service');
 
 test('login', async() =>{
     const login = {email: "a@jwt.com", password: "admin"};
-    loginRes = await request(app).put('/api/auth').send(login);
+    const loginRes = await request(app).put('/api/auth').send(login);
     expect(loginRes.statusCode).toBe(200);
     expect(loginRes.body.user.id).toBe(1);
     expect(loginRes.body.user.email).toMatch("a@jwt.com");
