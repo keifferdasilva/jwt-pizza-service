ab2c6b8a9092c7202f977927bb546239b1d18d9e
Keiffer da Silva <keifferdasilva@gmail.com>
2024-10-01 13:53:51 -0600

    add login test

--- src/tests/authRouter.test.js
+++ src/tests/authRouter.test.js
@@ -1,3 +1,12 @@
 const request = require('supertest')
 const app = require('../service');
 
+test('login', async() =>{
+    const login = {email: "a@jwt.com", password: "admin"};
+    loginRes = await request(app).put('/api/auth').send(login);
+    expect(loginRes.statusCode).toBe(200);
+    expect(loginRes.body.user.id).toBe(1);
+    expect(loginRes.body.user.email).toMatch("a@jwt.com");
+    expect(loginRes.body.user.roles).toEqual([{role: "admin"}]);
+    expect(loginRes.body).toHaveProperty('token');
+});
